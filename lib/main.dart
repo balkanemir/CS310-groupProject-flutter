dc21b7ee6dcf3317474ff237890cfc469eb71177
Emir Balkan <emir_balkan@hotmail.com>
2022-05-12 00:36:18 +0300

    firebase installed, screensizes added, mainpage updated

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,17 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:flutterui/routes/login.dart';
 import 'package:flutterui/routes/mainpage.dart';
 import 'package:flutterui/routes/shuffle.dart';
 import 'package:flutterui/routes/signup.dart';
+import 'firebase_options.dart';
 
-void main() {
+
+Future main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
   runApp(MaterialApp(
       initialRoute: '/mainpage',
       routes: {
--- lib/ui/post_card.dart
+++ lib/ui/post_card.dart
@@ -1,50 +1,63 @@
 import 'package:flutter/material.dart';
 import 'package:flutterui/models/User.dart';
 import 'package:flutterui/utils/colors.dart';
-
+import 'package:flutterui/utils/screensizes.dart';
+import 'package:like_button/like_button.dart';
 
 class PostCard extends StatelessWidget {
-  final User user; 
+  final User user;
   PostCard({required this.user});
-
 
   @override
   Widget build(BuildContext context) {
     return Center(
       child: Padding(
-        padding: const EdgeInsets.symmetric(horizontal: 30.0),
+        padding: const EdgeInsets.symmetric(horizontal: 10.0),
         child: Container(
-          margin: EdgeInsets.only(top: 8.0, bottom: 8.0),
-            width: 350,
-            height: 150,
+            margin: EdgeInsets.only(top: 8.0, bottom: 8.0),
+            width: screenSize(context).width,
+            height: screenHeight(context, dividedBy: 5),
             decoration: BoxDecoration(
-                color: primaryPinkDark,
+                border: Border.all(width: 1.0),
                 borderRadius: BorderRadius.circular(10.0)),
             child: Padding(
-              padding: const EdgeInsets.all(12.0),
+              padding: const EdgeInsets.all(8.0),
               child: Column(
-                children: [
-                Row(
-                    mainAxisAlignment: MainAxisAlignment.start,
-                    children: [
-                      ElevatedButton(
-                          onPressed: () {},
-                          child: CircleAvatar(
-                              radius: 25,
-                              backgroundColor: secondaryPinkLight,
-                              backgroundImage: NetworkImage(user.profile_image
-                                )),
-                          style: ElevatedButton.styleFrom(
-                            primary: secondaryPink800,
-                            shape: CircleBorder(),
-                          )),
-                      Text(user.username, style: TextStyle(
-                        color: textOnPrimaryBlack,
-                        fontSize: 14.0,
-                        fontWeight: FontWeight.bold,
-                      ),)
-                    ])
-              ]),
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.start,
+                      children: [
+                        ElevatedButton(
+                            onPressed: () {},
+                            child: CircleAvatar(
+                                radius: 20,
+                                backgroundColor: secondaryPinkLight,
+                                backgroundImage:
+                                    NetworkImage(user.profile_image)),
+                            style: ElevatedButton.styleFrom(
+                              primary: secondaryPink800,
+                              shape: CircleBorder(),
+                            )),
+                        Text(
+                          user.username,
+                          style: TextStyle(
+                            color: textOnPrimaryBlack,
+                            fontSize: 14.0,
+                            fontWeight: FontWeight.bold,
+                          ),
+                        )
+                      ],
+                    ),
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                      children: [
+                        LikeButton(
+                          size: 20,
+                        ),
+                      ],
+                    )
+                  ]),
             )),
       ),
     );

a4905c3c5fbf2ead293702eb2a713488d6274411
DatAmstrongest <93247554+DatAmstrongest@users.noreply.github.com>
2022-05-17 15:40:19 +0300

    Validation is added to editProfile

--- lib/routes/editProfile.dart
+++ lib/routes/editProfile.dart
@@ -1,19 +1,36 @@
+import 'dart:ffi';
+
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:email_validator/email_validator.dart';
+import 'dart:io' show Platform;
 
 import 'package:flutterui/utils/colors.dart';
 import 'package:flutterui/models/User.dart';
 
 class EditProfile extends StatefulWidget {
   final User user;
+  final Function updateName;
+  final Function updateSurname;
+  final Function updateUsername;
+  final Function updateEmail;
+  final Function updateMbti;
 
-  EditProfile(this.user);
+  EditProfile(this.user, this.updateName, this.updateSurname,
+      this.updateUsername, this.updateEmail, this.updateMbti);
 
   @override
   State<EditProfile> createState() => _EditProfileState();
 }
 
 class _EditProfileState extends State<EditProfile> {
-  String dropdownValue = "";
+  String mbtiValue = "";
+  String name = "";
+  String surname = "";
+  String username = "";
+  String email = "";
+  final _formKey = GlobalKey<FormState>();
+
   List<String> mbtiTypes = [
     "INTJ",
     "INTP",
@@ -32,9 +49,62 @@ class _EditProfileState extends State<EditProfile> {
     "ESTP",
     "ESFP"
   ];
+
+  Future<void> _showDialog(String title, String message) async {
+    bool isAndroid = Platform.isAndroid;
+    return showDialog(
+        context: context,
+        barrierDismissible: false,
+        builder: (BuildContext context) {
+          if (isAndroid) {
+            return AlertDialog(
+              title: Text(title),
+              content: SingleChildScrollView(
+                child: ListBody(
+                  children: [
+                    Text(message),
+                  ],
+                ),
+              ),
+              actions: [
+                TextButton(
+                  child: Text('OK'),
+                  onPressed: () {
+                    Navigator.of(context).pop();
+                  },
+                )
+              ],
+            );
+          } else {
+            return CupertinoAlertDialog(
+              title: Text(title),
+              content: SingleChildScrollView(
+                child: ListBody(
+                  children: [
+                    Text(message),
+                  ],
+                ),
+              ),
+              actions: [
+                TextButton(
+                  child: Text('OK'),
+                  onPressed: () {
+                    Navigator.of(context).pop();
+                  },
+                )
+              ],
+            );
+          }
+        });
+  }
+
   @override
   void initState() {
-    dropdownValue = widget.user.MBTI_type;
+    mbtiValue = widget.user.MBTI_type;
+    name = widget.user.name;
+    email = widget.user.email;
+    surname = widget.user.surname;
+    username = widget.user.username;
   }
 
   @override
@@ -57,170 +127,209 @@ class _EditProfileState extends State<EditProfile> {
           child: Padding(
             padding: EdgeInsets.only(left: 15),
             child: SingleChildScrollView(
-              child: Column(
-                children: [
-                  Center(
-                    child: Padding(
-                      padding: const EdgeInsets.only(top: 10),
-                      child: CircleAvatar(
-                        radius: 50,
-                        backgroundColor: secondaryPinkLight,
-                        backgroundImage: NetworkImage(
-                          widget.user.profile_image,
+              child: Form(
+                key: _formKey,
+                child: Column(
+                  children: [
+                    Center(
+                      child: Padding(
+                        padding: const EdgeInsets.only(top: 10),
+                        child: CircleAvatar(
+                          radius: 50,
+                          backgroundColor: secondaryPinkLight,
+                          backgroundImage: NetworkImage(
+                            widget.user.profile_image,
+                          ),
                         ),
                       ),
                     ),
-                  ),
-                  Divider(
-                    thickness: 1,
-                    color: Colors.cyan,
-                  ),
-                  Row(
-                    children: [
-                      SizedBox(
-                        width: 120,
-                        child: Text(
-                          "Name",
-                          style: TextStyle(
-                            color: Color.fromRGBO(87, 10, 87, 1),
-                            fontWeight: FontWeight.bold,
-                            fontSize: 20,
+                    Divider(
+                      thickness: 1,
+                      color: Colors.cyan,
+                    ),
+                    Row(
+                      children: [
+                        SizedBox(
+                          width: 120,
+                          child: Text(
+                            "Name",
+                            style: TextStyle(
+                              color: Color.fromRGBO(87, 10, 87, 1),
+                              fontWeight: FontWeight.bold,
+                              fontSize: 20,
+                            ),
                           ),
                         ),
-                      ),
-                      ConstrainedBox(
-                        constraints: BoxConstraints.tight(const Size(250, 50)),
-                        child: TextFormField(
-                          onChanged: (String value) {
-                            widget.user.name = value;
-                          },
-                          controller: TextEditingController(
-                              text: "${widget.user.name}"),
-                        ),
-                      )
-                    ],
-                  ),
-                  SizedBox(height: 20),
-                  Row(
-                    children: [
-                      SizedBox(
-                        width: 120,
-                        child: Text(
-                          "Surname",
-                          style: TextStyle(
-                            color: Color.fromRGBO(87, 10, 87, 1),
-                            fontWeight: FontWeight.bold,
-                            fontSize: 20,
+                        ConstrainedBox(
+                          constraints:
+                              BoxConstraints.tight(const Size(250, 50)),
+                          child: TextFormField(
+                            onChanged: (String value) {
+                              name = value;
+                            },
+                            controller: TextEditingController(text: name),
+                            onSaved: (value) {
+                              widget.updateName(value);
+                            },
+                          ),
+                        )
+                      ],
+                    ),
+                    SizedBox(height: 20),
+                    Row(
+                      children: [
+                        SizedBox(
+                          width: 120,
+                          child: Text(
+                            "Surname",
+                            style: TextStyle(
+                              color: Color.fromRGBO(87, 10, 87, 1),
+                              fontWeight: FontWeight.bold,
+                              fontSize: 20,
+                            ),
                           ),
                         ),
-                      ),
-                      ConstrainedBox(
-                        constraints: BoxConstraints.tight(const Size(250, 50)),
-                        child: TextFormField(
-                          onChanged: (String value) {
-                            widget.user.surname = value;
-                          },
-                          controller: TextEditingController(
-                              text: "${widget.user.surname}"),
-                        ),
-                      )
-                    ],
-                  ),
-                  SizedBox(height: 20),
-                  Row(
-                    children: [
-                      SizedBox(
-                        width: 120,
-                        child: Text(
-                          "Username",
-                          style: TextStyle(
-                            color: Color.fromRGBO(87, 10, 87, 1),
-                            fontWeight: FontWeight.bold,
-                            fontSize: 20,
+                        ConstrainedBox(
+                          constraints:
+                              BoxConstraints.tight(const Size(250, 50)),
+                          child: TextFormField(
+                            onChanged: (String value) {
+                              surname = value;
+                            },
+                            controller: TextEditingController(
+                              text: surname,
+                            ),
+                            onSaved: (value) {
+                              widget.updateSurname(value);
+                            },
+                          ),
+                        )
+                      ],
+                    ),
+                    SizedBox(height: 20),
+                    Row(
+                      children: [
+                        SizedBox(
+                          width: 120,
+                          child: Text(
+                            "Username",
+                            style: TextStyle(
+                              color: Color.fromRGBO(87, 10, 87, 1),
+                              fontWeight: FontWeight.bold,
+                              fontSize: 20,
+                            ),
                           ),
                         ),
-                      ),
-                      ConstrainedBox(
-                        constraints: BoxConstraints.tight(const Size(250, 50)),
-                        child: TextFormField(
-                          onChanged: (String value) {
-                            widget.user.username = value;
-                          },
-                          controller: TextEditingController(
-                              text: "${widget.user.username}"),
-                        ),
-                      )
-                    ],
-                  ),
-                  SizedBox(height: 20),
-                  Row(
-                    children: [
-                      SizedBox(
-                        width: 120,
-                        child: Text(
-                          "Email",
-                          style: TextStyle(
-                            color: Color.fromRGBO(87, 10, 87, 1),
-                            fontWeight: FontWeight.bold,
-                            fontSize: 20,
+                        ConstrainedBox(
+                          constraints:
+                              BoxConstraints.tight(const Size(250, 50)),
+                          child: TextFormField(
+                            onChanged: (String value) {
+                              username = value;
+                            },
+                            controller: TextEditingController(
+                              text: username,
+                            ),
+                            onSaved: (value) {
+                              widget.updateUsername(value);
+                            },
+                          ),
+                        )
+                      ],
+                    ),
+                    SizedBox(height: 20),
+                    Row(
+                      children: [
+                        SizedBox(
+                          width: 120,
+                          child: Text(
+                            "Email",
+                            style: TextStyle(
+                              color: Color.fromRGBO(87, 10, 87, 1),
+                              fontWeight: FontWeight.bold,
+                              fontSize: 20,
+                            ),
                           ),
                         ),
-                      ),
-                      ConstrainedBox(
-                        constraints: BoxConstraints.tight(const Size(250, 50)),
-                        child: TextFormField(
-                          onChanged: (String value) {
-                            widget.user.email = value;
-                          },
-                          controller: TextEditingController(
-                              text: "${widget.user.email}"),
-                        ),
-                      )
-                    ],
-                  ),
-                  SizedBox(height: 20),
-                  Row(
-                    children: [
-                      SizedBox(
-                        width: 120,
-                        child: Text(
-                          "MBTI Type",
-                          style: TextStyle(
-                            color: Color.fromRGBO(87, 10, 87, 1),
-                            fontWeight: FontWeight.bold,
-                            fontSize: 20,
+                        ConstrainedBox(
+                          constraints:
+                              BoxConstraints.tight(const Size(250, 50)),
+                          child: TextFormField(
+                            onChanged: (String value) {
+                              email = value;
+                            },
+                            controller: TextEditingController(
+                              text: email,
+                            ),
+                            validator: (value) {
+                              if (value != null) {
+                                if (value.isEmpty) {
+                                  return 'Cannot leave e-mail empty';
+                                }
+                                if (!EmailValidator.validate(value)) {
+                                  return 'Please enter a valid e-mail address';
+                                }
+                              }
+                            },
+                            onSaved: (value) {
+                              widget.updateEmail(value);
+                            },
+                          ),
+                        )
+                      ],
+                    ),
+                    SizedBox(height: 20),
+                    Row(
+                      children: [
+                        SizedBox(
+                          width: 120,
+                          child: Text(
+                            "MBTI Type",
+                            style: TextStyle(
+                              color: Color.fromRGBO(87, 10, 87, 1),
+                              fontWeight: FontWeight.bold,
+                              fontSize: 20,
+                            ),
                           ),
                         ),
-                      ),
-                      SizedBox(
-                        width: 250,
-                        child: DropdownButton<String>(
-                          value: dropdownValue,
-                          icon: const Icon(Icons.arrow_downward),
-                          elevation: 16,
-                          style: const TextStyle(color: Colors.deepPurple),
-                          underline: Container(
-                            height: 2,
-                            color: Colors.deepPurpleAccent,
+                        SizedBox(
+                          width: 250,
+                          child: DropdownButtonFormField<String>(
+                            value: mbtiValue,
+                            icon: const Icon(Icons.arrow_downward),
+                            elevation: 16,
+                            style: const TextStyle(color: Colors.deepPurple),
+                            onChanged: (String? value) {
+                              mbtiValue = value!;
+                            },
+                            onSaved: (value) {
+                              widget.updateMbti(value);
+                            },
+                            items: mbtiTypes
+                                .map<DropdownMenuItem<String>>((String value) {
+                              return DropdownMenuItem<String>(
+                                value: value,
+                                child: Text(value),
+                              );
+                            }).toList(),
                           ),
-                          onChanged: (newValue) {
-                            setState(() {
-                              dropdownValue = newValue!;
-                              widget.user.MBTI_type = newValue;
-                            });
-                          },
-                          items: mbtiTypes
-                              .map<DropdownMenuItem<String>>((String value) {
-                            return DropdownMenuItem<String>(
-                              value: value,
-                              child: Text(value),
-                            );
-                          }).toList(),
-                        ),
-                      )
-                    ],
-                  ),
-                ],
+                        )
+                      ],
+                    ),
+                    OutlinedButton(
+                      onPressed: () {
+                        if (_formKey.currentState!.validate()) {
+                          print('Email: $email');
+                          _formKey.currentState!.save();
+                          print('Email: $email');
+                        } else {
+                          _showDialog('Form Error', 'Your form is invalid');
+                        }
+                      },
+                      child: Text("Submit Changes"),
+                    ),
+                  ],
+                ),
               ),
             ),
           ),
